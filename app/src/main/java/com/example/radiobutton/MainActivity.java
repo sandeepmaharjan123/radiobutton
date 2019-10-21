df02921c5240a43d9c2167a063d7e9b1701e7e0e
Sandeep Maharjan <ssnipmhrz@gmail.com>
2019-10-21 09:49:42 +0545

    Initial commit

---
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.radiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    RadioButton radioButton1,radioButton2,radioButton3;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioButton1 = findViewById(R.id.rbBird);
+        radioButton2 = findViewById(R.id.rbTiger);
+        radioButton3 = findViewById(R.id.rbSnake);
+        imageView = findViewById(R.id.ivShowing);
+
+        radioButton1.setOnClickListener(this);
+        radioButton2.setOnClickListener(this);
+        radioButton3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.rbBird :
+                imageView.setImageResource(R.drawable.eagle);
+                break;
+            case R.id.rbTiger :
+                imageView.setImageResource(R.drawable.tiger);
+                break;
+            case R.id.rbSnake :
+                imageView.setImageResource(R.drawable.snake);
+                break;
+        }
+    }
+}
